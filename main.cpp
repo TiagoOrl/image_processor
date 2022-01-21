5e7f0b39f44c1d8adee01d83b6a2ef3adca36fe2
tiago <tiago@pop-os.localdomain>
2022-01-21 11:46:15 -0300

    Code refacored

--- main.cpp
+++ main.cpp
@@ -11,27 +11,31 @@
 
 // uso / usage (linux)
 
-// compilação, comile with:
+// install libopencv-dev
+
+// compilação, compile with:
 // g++ -o process main.cpp -I /usr/include/opencv4 -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc
 
 // run / rodar:
 // process filename
 
 void blur_gauss(unsigned char * channel_input, unsigned char &p, int index, int colSize);
-void sobel_edgeDetect(unsigned char * channel_input, unsigned char & p, int index, int colSize);
-void threshold(unsigned char & p);
-void filter_contrast(unsigned char & p);
+void sobel_edgeDetect(cv::Mat &imgInput, cv::Mat &imgOutput);
+void threshold (cv::Mat &imgInput, cv::Mat &imgOut);
+void filter_contrast(cv::Mat &imgInput, cv::Mat &imgOut);
 
+std::string selectedFilter;
 
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2){
-        printf("usage: process imgfilename");
+    if (argc != 3){
+        printf("usage: process --type imgfilename");
         return EXIT_FAILURE;
     }
 
-    std::string image_path = cv::samples::findFile(argv[1]);
+    std::string image_path = cv::samples::findFile(argv[2]);
+    selectedFilter = argv[1];
     cv::Mat img_input = cv::imread(image_path, cv::IMREAD_REDUCED_COLOR_4);
 
 
@@ -40,112 +44,185 @@ int main(int argc, char const *argv[])
         std::cout << "Could not read the image: " << image_path << std::endl;
         return 1;
     }
-
-    cv::cvtColor(img_input, img_input, cv::COLOR_RGBA2RGB);
-
-    cv::Mat rgb[3];
-    cv::Mat new_rgb[3];
-
-    // create copies of each 3 channel into a Mat[3] array
-    cv::split(img_input, rgb);
-    cv::split(img_input, new_rgb);
-
-    // then we get the pointer ref from each channel from the Mat[3] into a uchar * pointer/array
-    uchar *c_blue = rgb[0].data;
-    uchar *c_green = rgb[1].data;
-    uchar *c_red = rgb[2].data;
-
-    // same as before, but in new copies to modify it and then create a new cv::Mat object
-    uchar *c_newBlue = new_rgb[0].data;
-    uchar *c_newGreen = new_rgb[1].data;
-    uchar *c_newRed = new_rgb[2].data;
+    
+    cv::Mat imgOutput;
 
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < img_input.rows; i++)
-        for (int j = 0;j < img_input.cols; j++) {
 
-            int index = i * img_input.cols + j;
+    if (selectedFilter.compare("--thre") == 0)
+        threshold(img_input, imgOutput);
 
-            // here is where we process each channel, uncomment/comment then to change the filter you want to use
-            // to apply a filter to an image, you have to apply the filter to each of the 3 channel image
+    else if (selectedFilter.compare("--contrast") == 0) {
+        filter_contrast(img_input, imgOutput);
 
-            threshold(c_red[index]);
-            threshold(c_green[index]);
-            threshold(c_blue[index]);
+    } 
 
-            // sobel_edgeDetect(c_red, c_newRed[index], index, img_input.cols);
-            // sobel_edgeDetect(c_green, c_newGreen[index], index, img_input.cols);
-            // sobel_edgeDetect(c_blue, c_newBlue[index], index, img_input.cols);
+    else if (selectedFilter.compare("--sobel") == 0) {
+        sobel_edgeDetect(img_input, imgOutput);
+    }
 
-            // filter_contrast(c_blue[index]);
-            // filter_contrast(c_red[index]);
-            // filter_contrast(c_green[index]);
+    else {
+        printf("invalid filter param\nsupported params: --constrast, --thre --sobel \naborting...\n\n");
+        return 1;
+    }
 
-        }
 
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     
-    printf("Tempo decorrido/ time passed: %ld ms \n", duration.count());
-    
-    cv::Mat output_img;
-    cv::merge(new_rgb, 3, output_img);
+    printf("Tempo decorrido\n time passed: %ld ms \n", duration.count());
+
         
-    cv::imshow("Img Output::", output_img);
-
-
+    cv::imshow("Img Output", imgOutput);
     char k;
 
-    while (k != 'q'){
+    while (k != 'q') {
         k = cv::waitKey(0); // img_input for a keystroke in the window
     }
 
     return 0;
 }
 
-void filter_contrast(unsigned char & channel_pixel) {
-    if (channel_pixel > 240) channel_pixel *= 0.98;
-    if (channel_pixel < 30) channel_pixel *= 2.356;
+void filter_contrast (cv::Mat &imgInput, cv::Mat &imgOut) {
+
+    cv::cvtColor(imgInput, imgInput, cv::COLOR_RGBA2RGB);
+
+    cv::Mat inputChannels[3];
+    cv::Mat outChannels[3];
+
+    cv::split(imgInput, inputChannels);
+    cv::split(imgInput, outChannels);
+
+    uchar *inputBlue = inputChannels[0].data;
+    uchar *inputGreen = inputChannels[1].data;
+    uchar *inputRed = inputChannels[2].data;
+
+    uchar *outBlue = outChannels[0].data;
+    uchar *outGreen = outChannels[1].data;
+    uchar *outRed = outChannels[2].data;
+
+    for (int i = 0; i < imgInput.rows; i++)
+        for (int j = 0;j < imgInput.cols; j++) {
+
+            int index = i * imgInput.cols + j;
+
+            if (inputRed[index] > 240) 
+                outRed[index] *= 0.98;
+            if (inputRed[index] < 30) 
+                outRed[index] *= 2.356;
+
+            if (inputGreen[index] > 240) 
+                outGreen[index] *= 0.98;
+            if (inputGreen[index] < 30) 
+                outGreen[index] *= 2.356;
+
+            if (inputBlue[index] > 240) 
+                outBlue[index] *= 0.98;
+            if (inputBlue[index] < 30) 
+                outBlue[index] *= 2.356;
+
+        }
+
+    cv::merge(outChannels, sizeof(outChannels)/sizeof(*outChannels), imgOut);
 }
 
 
-void threshold(unsigned char & channel_pixel){
-    if (channel_pixel > 210) channel_pixel = 200;
-    if (channel_pixel < 30) channel_pixel = 10;
+void threshold (cv::Mat &imgInput, cv::Mat &imgOut) {
+
+    cv::cvtColor(imgInput, imgInput, cv::COLOR_RGBA2RGB);
+
+    cv::Mat inputChannels[3];
+    cv::Mat outChannels[3];
+
+    cv::split(imgInput, inputChannels);
+    cv::split(imgInput, outChannels);
+
+    uchar *inputBlue = inputChannels[0].data;
+    uchar *inputGreen = inputChannels[1].data;
+    uchar *inputRed = inputChannels[2].data;
+
+    uchar *outBlue = outChannels[0].data;
+    uchar *outGreen = outChannels[1].data;
+    uchar *outRed = outChannels[2].data;
+
+    for (int i = 0; i < imgInput.rows; i++)
+        for (int j = 0;j < imgInput.cols; j++) {
+
+            int index = i * imgInput.cols + j;
+
+            if (inputRed[index] > 100) 
+                outRed[index] = 255;
+            else
+                outRed[index] = 10;
+
+            if (inputGreen[index] > 100) 
+                outGreen[index] = 255;
+            else
+                outGreen[index] = 10;
+
+            if (inputBlue[index] > 100) 
+                outBlue[index] = 255;
+            else
+                outBlue[index] = 10;
+        }
+
+    cv::merge(outChannels, sizeof(outChannels)/sizeof(*outChannels), imgOut);
 }
 
-void sobel_edgeDetect(unsigned char * channel_input, unsigned char & c_pixel, int index, int colSize) {
 
-    int v_kernel = (
-        channel_input[index] * 0.0 +
+void sobel_edgeDetect(cv::Mat &imgInput, cv::Mat &imgOutput) {
+
+    cv::cvtColor(imgInput, imgInput, cv::COLOR_RGBA2GRAY);
+
+    cv::Mat inputChannels[1];
+    cv::Mat outChannels[1];
+
+    cv::split(imgInput, inputChannels);
+    cv::split(imgInput, outChannels);
+
+    uchar *inputGrey = inputChannels[0].data;
+
+    uchar *outGrey = outChannels[0].data;
+
+    int colSize = imgInput.cols;
+
+    for (int i = 0; i < imgInput.rows; i++)
+        for (int j = 0;j < imgInput.cols; j++) {
+
+            int index = i * imgInput.cols + j;
+
+            int v_kernel = (
+                inputGrey[index] * 0.0 +
+                        
+                inputGrey[index - 1] * 2.0 +     // west
+                inputGrey[index + 1] * -2.0 +     // east
                 
-        channel_input[index - 1] * 2.0 +     // west
-        channel_input[index + 1] * -2.0 +     // east
-        
-        channel_input[index - colSize] * 0.0 +    // north
-        channel_input[index + colSize] * 0.0 +    // south
-        
-        channel_input[index - colSize - 1] * 1.0 +    // northwest
-        channel_input[index - colSize + 1] * -1.0 +    // northeast
-        channel_input[index + colSize - 1] * 1.0 +    // southwest
-        channel_input[index + colSize + 1] * -1.0 ) ;   // southeast
-
-    int h_kernel = (
-        channel_input[index] * 0.0 +
+                inputGrey[index - colSize] * 0.0 +    // north
+                inputGrey[index + colSize] * 0.0 +    // south
                 
-        channel_input[index - 1] * 0.0 +     // west
-        channel_input[index + 1] * 0.0 +     // east
-        
-        channel_input[index - colSize] * 2.0 +    // north
-        channel_input[index + colSize] * -2.0 +    // south
-        
-        channel_input[index - colSize - 1] * 1.0 +    // northwest
-        channel_input[index - colSize + 1] * 1.0 +    // northeast
-        channel_input[index + colSize - 1] * -1.0 +    // southwest
-        channel_input[index + colSize + 1] * -1.0 ) ;   // southeast
+                inputGrey[index - colSize - 1] * 1.0 +    // northwest
+                inputGrey[index - colSize + 1] * -1.0 +    // northeast
+                inputGrey[index + colSize - 1] * 1.0 +    // southwest
+                inputGrey[index + colSize + 1] * -1.0 ) ;   // southeast
 
-        c_pixel = (unsigned char) std::abs( (h_kernel + v_kernel) / 2);
-    
+            int h_kernel = (
+                inputGrey[index] * 0.0 +
+                        
+                inputGrey[index - 1] * 0.0 +     // west
+                inputGrey[index + 1] * 0.0 +     // east
+                
+                inputGrey[index - colSize] * 2.0 +    // north
+                inputGrey[index + colSize] * -2.0 +    // south
+                
+                inputGrey[index - colSize - 1] * 1.0 +    // northwest
+                inputGrey[index - colSize + 1] * 1.0 +    // northeast
+                inputGrey[index + colSize - 1] * -1.0 +    // southwest
+                inputGrey[index + colSize + 1] * -1.0 ) ;   // southeast
+
+            outGrey[index] = (unsigned char) std::abs( (h_kernel + v_kernel) / 2);
+    }
+
+    cv::merge(outChannels, sizeof(outChannels)/sizeof(*outChannels), imgOutput);
 }
 
 void blur_gauss(unsigned char * channel_input, unsigned char & c_pixel, int index, int colSize) {
